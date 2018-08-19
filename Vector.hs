0425fa92ba9401dd1dca922f868c58b2d3215ffe
Ignat Insarov <kindaro@gmail.com>
2018-08-19 18:47:29 +0700

    Convert to TypeLits.

--- Vector.hs
+++ Vector.hs
@@ -10,16 +10,12 @@ module Vector
   where
 
 import Prelude hiding (zip)
+import GHC.TypeLits
 
-data N
+data Vector (n :: Nat) a
   where
-    Z :: N
-    S :: N -> N
-
-data Vector (n :: N) a
-  where
-    VZ :: Vector Z a
-    (:::) :: a -> Vector n a -> Vector (S n) a
+    VZ :: Vector 0 a
+    (:::) :: a -> Vector n a -> Vector (n + 1) a
 
 infixr 1 :::
 
@@ -29,17 +25,17 @@ class Zip z
   where
     zip :: z a -> z b -> z (a, b)
 
-instance Zip (Vector n) => Zip (Vector (S n))
+instance Zip (Vector n) => Zip (Vector (n + 1))
   where
     zip (x ::: xs) (y ::: ys) = (x, y) ::: zip xs ys
 
-instance Zip (Vector Z)
+instance Zip (Vector 0)
   where
     zip _ _ = VZ
 
 -- ^
 -- λ :t zip (1 ::: 2 ::: 3 ::: VZ) (4 ::: 5 ::: 6 ::: VZ)
 -- zip (1 ::: 2 ::: 3 ::: VZ) (4 ::: 5 ::: 6 ::: VZ)
---   :: (Num a, Num b) => Vector ('S ('S ('S 'Z))) (a, b)
+--   :: (Num a, Num b) => Vector 3 (a, b)
 -- λ zip (1 ::: 2 ::: 3 ::: VZ) (4 ::: 5 ::: 6 ::: VZ)
 -- (1,4) ::: ((2,5) ::: ((3,6) ::: VZ))
